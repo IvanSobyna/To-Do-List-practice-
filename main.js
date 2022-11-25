c4ff1166292c4b145ed13035a21d79fac6da1796
IvanSobyna <sobynaivan@gmail.com>
2022-11-25 22:11:21 +0200

    add options

--- main.js
+++ main.js
@@ -4,26 +4,40 @@ let taskList = document.querySelector(".task-list");
 
 newTaskBtn.addEventListener("click", addTask);
 taskList.addEventListener("click", changeTask);
-
+taskList.addEventListener("click", doneDel);
 
 function createTask(e){
     let div = document.createElement("div");
     div.classList.add("taskOne");
 
-    let input = document.createElement("input");
+    let input = document.createElement("div");
 
-    input.type = "checkbox";
+    input.type = "";
 
     let p = document.createElement("p");
     p.innerText = e;
 
+    let completebtn = document.createElement("button");
+    completebtn.innerHTML = `✔️`;
+    completebtn.classList.add("completebtn");
+
+    let deletebtn = document.createElement("button");
+    deletebtn.innerHTML = `✖️`;
+    deletebtn.classList.add("deletebtn");
+
     div.append(input);
     div.append(p);
+    div.append(completebtn);
+    div.append(deletebtn);
+
+
     return div;
 }
 function addTask(){
     let newTask = createTask(inputTask.value);
     taskList.append(newTask);
+
+
     inputTask.value = "";
 };
 
@@ -35,21 +49,23 @@ else {
     e.target.parentElement.classList.remove("completed");
 }
 }
+function doneDel(e) {
+    const option = e.target;
 
-// let todoList = [];
-// newTaskBtn.addEventListener("click", function (){
-//
-//     let newTodo = {
-//         todo: input.value,
-//         checked: false
-//     };
-//
-//     todoList.push(newTodo);
-//     showMessage()
-// });
-// function showMessage(){
-//     todoList.forEach(function (){
-//         taskList.append(input.value);
-//         input.value = "";
-//     })
-// }
+    if (option.classList[0] === "deletebtn") {
+        let optionToDo = option.parentElement;
+        optionToDo.remove();
+    }
+    if (option.classList[0] === "completebtn") {
+        let optionToDo = option.parentElement;
+        optionToDo.classList.toggle("completed");
+    }
+}
+
+inputTask.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        newTaskBtn.click();
+    }
+});
+
